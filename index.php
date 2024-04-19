1c7fbedf652678e6fddfe13043076d0640051780
Nam khánh <81094544+Nkhanh218@users.noreply.github.com>
2024-04-19 07:33:14 +0000

    second commit

--- index.php
+++ index.php
@@ -25,6 +25,12 @@ function sayHello($name) {
 			echo "$p </br>";
 			echo $s;
 			//ok
+			for($i=1;$i<=200;$i++){
+				if($i%2==0)
+					echo "<b><font color=red>".$i."</font></b>";
+			else
+					echo "<i><font color=blue>".$i."</font></i>";
+			}
 		?>
 	</body>
 </html>
